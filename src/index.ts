0a0a1f805ac1fc344b0107366a53fdcd0aa5f405
omaroskars <omar@konstant.is>
2025-02-11 11:32:43 +0100

    export gutter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/grid'
 export * from './components/stack'
+export * from './components/gutter'
 
 export type { BreakpointValues, ResponsiveStyle, ResponsiveValue, UIComponentProps } from './types'
